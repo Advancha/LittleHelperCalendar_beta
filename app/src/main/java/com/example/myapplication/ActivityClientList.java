cbbdcc91fff3c439782ae3c747b0643771ac0300
Advancha <advancha@gmail.com>
2017-02-27 19:49:06 +0200

    swipe to dismiss from lient list

--- app/src/main/java/com/example/myapplication/ActivityClientList.java
+++ app/src/main/java/com/example/myapplication/ActivityClientList.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
@@ -30,23 +31,19 @@ import jp.wasabeef.recyclerview.animators.SlideInRightAnimator;
  * Created by Ира on 01.02.2017.
  */
 
-public class ActivityClientList extends AppCompatActivity {
+public class ActivityClientList extends AppCompatActivity implements OnStartDragListener{
     public RecyclerView rvClientList;
     public Cursor cursor;
     private RecyclerView.LayoutManager mLayoutManager;
     ClientsCursorRecyclerAdapter cursorAdapter;
+
+    private ItemTouchHelper mItemTouchHelper;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_list);
 
-        TypedValue typedValue = new TypedValue();
-        Resources.Theme theme = this.getTheme();
-        theme.resolveAttribute(R.attr.colorBackgroundFloating, typedValue, true);
-        final int color_norm = typedValue.data;
-
-
-        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab_new_client);
 
         rvClientList = (RecyclerView) findViewById(R.id.client_list);
         mLayoutManager = new LinearLayoutManager(this);
@@ -56,96 +53,31 @@ public class ActivityClientList extends AppCompatActivity {
 
         rvClientList.setHasFixedSize(true);
 
-        ScaleInAnimator itemAnimator = new ScaleInAnimator();
+     //   ScaleInAnimator itemAnimator = new ScaleInAnimator();
      //   DefaultItemAnimator itemAnimator = new DefaultItemAnimator();
-        itemAnimator.setRemoveDuration(500);
-        rvClientList.setItemAnimator(itemAnimator);
+     //   itemAnimator.setRemoveDuration(500);
+     //   rvClientList.setItemAnimator(itemAnimator);
 
 
 
         final DBHelper dbHelper = new DBHelper(this);
         final Cursor cursor = dbHelper.getCursorForClientList();
 
-        cursorAdapter = new ClientsCursorRecyclerAdapter(cursor, dbHelper);
+        cursorAdapter = new ClientsCursorRecyclerAdapter(cursor, dbHelper, this);
         rvClientList.setAdapter(cursorAdapter);
-/*
-        rvClientList.setOnTouchListener(new View.OnTouchListener() {
-            float historicX = Float.NaN, historicY = Float.NaN, delX= Float.NaN, delY= Float.NaN;
-            LinearLayout childViewUnderv;
-            ImageView imageViewDelete;
-            int _id=-1;
 
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        historicX = event.getX();
-                        childViewUnderv = (LinearLayout)((RecyclerView)v).findChildViewUnder(historicX, historicY);
-                        imageViewDelete = (ImageView)(childViewUnderv.findViewById(R.id.list_cell_client_delete));
-
-                        childViewUnderv.setBackgroundColor(getResources().getColor(R.color.accent_light));
-
-                        _id=(int)childViewUnderv.getTag(R.id.TAG_CLIENT_ID);
-                        delX=imageViewDelete.getX();
-
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        imageViewDelete.setVisibility(View.VISIBLE);
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        if (event.getX() >= delX) {
-                            deletefromClientList(_id);
-                            return true;
-                        }
-                        else
-                        {
-                            imageViewDelete.setVisibility(View.INVISIBLE);
-                            childViewUnderv.setBackgroundColor(color_norm);
-
-                        }
-                        break;
-                    default:
-                        return false;
-                }
-                return false;
-            }
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(cursorAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(rvClientList);
 
 
-            public void deletefromClientList(int _id){
-                int _pos=-1;
-
-                Cursor curent_cursor = cursorAdapter.getCursor();
-                curent_cursor.moveToFirst();
-                for (int i=0; i<curent_cursor.getCount(); i++){
-                    if (curent_cursor.getInt(curent_cursor.getColumnIndexOrThrow(DBContract.TabClients._ID))==_id){
-                        _pos=curent_cursor.getPosition();
-                        break;
-                    }
-                    else {
-                        curent_cursor.moveToNext();
-                    }
-                }
-
-                dbHelper.deleteClient(_id);
-                Cursor new_cursor = dbHelper.getCursorForClientList();
-                cursorAdapter.changeCursorWithoutNotification(new_cursor);
-                cursorAdapter.notifyItemRemoved(_pos);
-
-            }
-
-        });
-
-*/
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab_new_client);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 addNewClient();
             }
         });
-
-
-
-
 
     }
 
@@ -155,7 +87,8 @@ public class ActivityClientList extends AppCompatActivity {
     }
 
 
-
-
-
+    @Override
+    public void onStartDrag(ClientsCursorRecyclerAdapter.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
+    }
 }
--- app/src/main/java/com/example/myapplication/ClientsCursorRecyclerAdapter.java
+++ app/src/main/java/com/example/myapplication/ClientsCursorRecyclerAdapter.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.AsyncTask;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -12,51 +16,76 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import static com.example.myapplication.R.*;
+
 /**
  * Created by Ира on 16.02.2017.
  */
 
 public class ClientsCursorRecyclerAdapter extends CursorRecyclerAdapter<ClientsCursorRecyclerAdapter.ViewHolder>
-
+             implements ItemTouchHelperAdapter
 
 {
     public DBHelper mDbHelper;
+    private final OnStartDragListener mDragStartListener;
 
-    public ClientsCursorRecyclerAdapter(Cursor cursor, DBHelper dbHelper){
+    public ClientsCursorRecyclerAdapter(Cursor cursor, DBHelper dbHelper, OnStartDragListener dragStartListener){
         super(cursor);
         this.mDbHelper = dbHelper;
+        this.mDragStartListener = dragStartListener;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public boolean onItemMove(int fromPosition, int toPosition) {
+        return false;
+    }
+
+    @Override
+    public void onItemDismiss(int position) {
+        int client_id = (int) getItemId(position);
+        mDbHelper.deleteClient(client_id);
+
+        Cursor new_cursor = mDbHelper.getCursorForClientList();
+        changeCursorWithoutNotification(new_cursor);
+        notifyItemRemoved(position);
+
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder{
         public LinearLayout llParent;
 
         public TextView tvName;
         public TextView tvPhone;
-        public ImageView ivDelete;
-
 
         public ViewHolder(LinearLayout l) {
             super(l);
 
             llParent = l;
-            tvName = (TextView)(l.findViewById(R.id.list_cell_client_name));
-            tvPhone = (TextView)(l.findViewById(R.id.list_cell_client_phone));
-            ivDelete = (ImageView) (l.findViewById(R.id.list_cell_client_delete));
+            tvName = (TextView)(l.findViewById(id.list_cell_client_name));
+            tvPhone = (TextView)(l.findViewById(id.list_cell_client_phone));
+           }
 
+        @Override
+        public void onItemSelected() {
+            itemView.setBackgroundColor(Color.LTGRAY);
+        }
 
+        @Override
+        public void onItemClear() {
+            itemView.setBackgroundColor(0);
         }
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LinearLayout l=(LinearLayout)LayoutInflater.from(parent.getContext()).inflate(R.layout.client_list_cell, parent, false);
+        LinearLayout l=(LinearLayout)LayoutInflater.from(parent.getContext()).inflate(layout.client_list_cell, parent, false);
         ViewHolder vh=new ViewHolder(l);
         return vh;
     }
 
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, final Cursor cursor) {
+    public void onBindViewHolder(final ViewHolder holder, final Cursor cursor) {
         String strName = cursor.getString(cursor.getColumnIndexOrThrow(DBContract.TabClients.COLUMN_NAME_NAME));
         String strPhone = cursor.getString(cursor.getColumnIndexOrThrow(DBContract.TabClients.COLUMN_NAME_PHONE));
         final int client_id = cursor.getInt(cursor.getColumnIndexOrThrow(DBContract.TabClients._ID));
@@ -64,77 +93,18 @@ public class ClientsCursorRecyclerAdapter extends CursorRecyclerAdapter<ClientsC
 
         holder.tvName.setText(strName);
         holder.tvPhone.setText(strPhone);
-        holder.llParent.setTag(R.id.TAG_CLIENT_ID, client_id);
-/*
+        holder.llParent.setTag(id.TAG_CLIENT_ID, client_id);
+
+        // Start a drag whenever the handle view it touched
         holder.llParent.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                float historicX = Float.NaN, historicY = Float.NaN, delX= Float.NaN, delY= Float.NaN;
-                LinearLayout childViewUnderv = (LinearLayout)((RecyclerView)v).findChildViewUnder(historicX, historicY);
-                ImageView imageViewDelete = (ImageView)(childViewUnderv.findViewById(R.id.list_cell_client_delete));
-
-
-                switch (event.getAction()) {
-                        case MotionEvent.ACTION_DOWN:
-                            historicX = event.getX();
-                            historicY = event.getY();
-
-                            delX=imageViewDelete.getX();
-                            delY = imageViewDelete.getY();
-                            break;
-                        case MotionEvent.ACTION_MOVE:
-                            imageViewDelete.setVisibility(View.VISIBLE);
-                            break;
-                        case MotionEvent.ACTION_UP:
-                            if (event.getX() >= delX) {
-                                deleteFromCursor(client_id, cursor_pos);
-                                return true;
-                            }
-                            else
-                            {
-                                imageViewDelete.setVisibility(View.INVISIBLE);
-                            }
-                            break;
-                        default:
-                            return false;
-                    }
-                    return false;
+                if (MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_DOWN) {
+                    mDragStartListener.onStartDrag(holder);
                 }
-
-            public void deleteFromCursor(int client_id, int cursor_pos){
-                mDbHelper.deleteClient(client_id);
-                Cursor new_cursor = mDbHelper.getCursorForClientList();
-                changeCursorWithoutNotification(new_cursor);
-                notifyItemRemoved(cursor_pos);
+                return false;
             }
         });
-        */
-        holder.ivDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                int cursor_pos=-1;
-
-                Cursor curent_cursor = getCursor();
-                curent_cursor.moveToFirst();
-                for (int i=0; i<curent_cursor.getCount(); i++){
-                    if (curent_cursor.getInt(curent_cursor.getColumnIndexOrThrow(DBContract.TabClients._ID))==client_id){
-                        cursor_pos=curent_cursor.getPosition();
-                        break;
-                    }
-                    else {
-                        curent_cursor.moveToNext();
-                    }
-                }
-
-                mDbHelper.deleteClient(client_id);
-                Cursor new_cursor = mDbHelper.getCursorForClientList();
-                changeCursorWithoutNotification(new_cursor);
-                notifyItemRemoved(cursor_pos);
-                //notifyItemRemoved(cursor_pos);
-            }
-        });
-
     }
 
 
